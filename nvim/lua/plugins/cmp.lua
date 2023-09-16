2b3f34a1d33f31d1034504528db113d3ab03aa54
ULeang <ULeang@outlook.com>
2023-09-16 09:59:11 +0800

    update

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -81,9 +81,9 @@ return {
     -- Set up lspconfig.
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-    -- require('lspconfig')['pytight'].setup {
-    --   capabilities = capabilities
-    -- }
+    require('lspconfig')['pyright'].setup {
+      capabilities = capabilities
+    }
     require('lspconfig')['clangd'].setup {
       capabilities = capabilities
     }
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -5,6 +5,7 @@ export ZDOTDIR="$HOME/.config/zsh"
 export MPD_HOST="$HOME/.config/mpd/socket"
 
 export EDITOR="nvim"
+export VISUAL="nvim"
 
 # my script dir
 export PATH="$HOME/.config/scripts:$PATH"
