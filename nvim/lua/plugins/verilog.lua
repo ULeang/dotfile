2a74c899abad5f5dfa6c5e7691652c86e892733d
ULeang <ULeang@outlook.com>
2024-05-17 22:08:46 +0800

    update

--- nvim/lua/plugins/verilog.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'vhda/verilog_systemverilog.vim',
-  ft = "verilog_systemverilog",
-  config = function()
-    vim.opt_local.smartindent = false
-    vim.opt_local.autoindent = true
-  end
-}
